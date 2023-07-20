572a8ab04873cc3da641f820672fa2ffac2a9d65
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-07-20 15:08:39 +0200

    update service API

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -14,54 +14,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class GameController extends AbstractController
 {
 
-     #[Route('/search/results', name: 'search_results', methods: ['GET'])]
-    public function searchResults(GameRepository $gameRepository, Request $request): Response
+    #[Route('game/recherche', name: 'search_results', methods: ['GET'])]
+    public function searchResults(Request $request, GiantBombAPI $giantBombAPI): Response
     {
-        $searchTerm = $request->query->get('searchTerm', '');
-        // Récupérez les résultats de recherche depuis la base de données
-        $games = $gameRepository->findAll();
+        $searchTerm = $request->query->get('game_name','');
 
-        // Affichez les résultats de recherche en utilisant un template Twig
+        $games = $giantBombAPI->searchGame($searchTerm);
         return $this->render('game/index.html.twig', ['games' => $games, 'searchTerm' => $searchTerm]);
     }
 
-    #[Route('/search-game', name: 'search_game', methods: ['GET'])]
-    public function searchGame(Request $request, GiantBombApi $giantBombApiService, EntityManagerInterface $entityManager): Response
+
+
+    #[Route('game/result/{guid}', name: 'game_show')]
+    public function show(string $guid, GiantBombAPI $giantBombAPI): Response
     {
-        // Récupérez le terme de recherche à partir de la requête GET
-        $gameName = $request->query->get('game_name');
-
-        // Utilisez le service GiantBombApiService pour rechercher le jeu
-        try {
-            $games = $giantBombApiService->searchGame($gameName, 1);
-
-            // Hydratez la base de données avec le premier jeu récupéré depuis l'API (limité à 1 résultat)
-            if (!empty($games)) {
-                $gameInfo = $games[0];
-
-                $game = new Game();
-                $game->setName($gameInfo['name']);
-                $game->setDescription($gameInfo['deck']);
-                $game->setPicture($gameInfo['image']['medium_url']);
-                $game->setDate($gameInfo['date_added']);
-                $entityManager->persist($game);
-                $entityManager->flush();
-            }
-
-            // Redirigez vers la page de résultats de recherche avec le terme de recherche en tant que paramètre GET
-            return $this->redirectToRoute('search_results', ['searchTerm' => $gameName]);
-
-        } catch (\Exception $e) {
-            // Gérez les erreurs, par exemple affichez un message d'erreur
-            return $this->json(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
-        }
-    }
-
-    #[Route('game/{id}', name: 'game_show')]
-    public function show(Game $game): Response
-    {
+        $game = $giantBombAPI->findGame($guid);
         return $this->render('game/show.html.twig', ['game' => $game]);
     }
-
-
 }
